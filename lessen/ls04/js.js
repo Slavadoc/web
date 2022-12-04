e55aaad5963fd3ac0a10fcbf4f9d32faf4babb35
Slavadoc <54536848+Slavadoc@users.noreply.github.com>
2022-12-04 21:24:20 +0300

    hw4

---
+++ lessen/ls04/js.js
@@ -0,0 +1,6 @@
+
+document.getElementById("var1").innerText = var1
+document.getElementById("var2").innerText = var2
+result = var1 + var2
+alert(result)
+document.getElementById("result").innerText = `{var1} +  {var2} = {result}`;
